ef71d978431a3ee43139db0f375bfa26f94446b9
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-19 01:57:39 +0100

    mandatory

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program prints to the standard output 
+ * this string (\"Programming is like building a multilingual puzzle)
+ * using puts
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
