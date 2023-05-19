74094207526aff4b32e96caa2ecd270085281af4
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-19 20:48:25 +0100

    #advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			putchar(a);
+			putchar(b);
+			if ((a != '8') || (b != '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
