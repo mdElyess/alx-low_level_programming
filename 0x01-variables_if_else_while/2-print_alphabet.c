5de0ccf55e40d8fc0921cd7e059ebe05e26c609e
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-19 19:42:58 +0100

    mandatory

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 'z' + 1; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
