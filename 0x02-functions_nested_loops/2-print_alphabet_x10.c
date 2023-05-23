78e08b60201de445553a8f0ec1468bf583a59def
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-23 21:18:11 +0100

    mandatory

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,12 @@
 
 void print_alphabet_x10()
 {
+	char a;
 	int i;
 
-	for (i = 0; i < 10; i++)
-		print_alphabet();
+	for (i = 0; i <10; i++)
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+		_putchar('\n');
+
 }
