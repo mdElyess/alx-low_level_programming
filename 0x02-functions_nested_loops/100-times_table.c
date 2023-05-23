0eb0a206f31c166bd80ba52179b0edd33cb7c5b2
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-23 23:21:08 +0100

    #advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,9 +10,9 @@ void print_times_table(int n)
 
 	if (n <= 15 && n >= 0)
 	{
-		for (a = 0; a < n; a++)
+		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b < n; b++)
+			for (b = 0; b <= n; b++)
 			{
 				if (b == 0)
 				{
