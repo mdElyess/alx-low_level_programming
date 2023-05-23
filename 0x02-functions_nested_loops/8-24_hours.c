fed1f8863a1219ab5a2f4ab35d14c89b445a6e90
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-23 22:36:55 +0100

    mandatory

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the hours from 00:00 to 23:59
+ **/
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+		}
+		_putchar('\n');
+	}
+	return (r);
+}
