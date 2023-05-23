9f0e1ba435dfd94e8092814e8ecbc77b2b3041d7
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-23 21:14:10 +0100

    mandatory

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10()
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
