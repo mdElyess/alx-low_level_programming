0a80d16dc845798daae66b6927684674937a520d
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-23 22:11:31 +0100

    mandatory

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if c is an alphabet
+ *
+ * @n: a signed number
+ *
+ * Return: 1 if n > 0, 0 if n = 0 and -1 if n <0
+ **/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
