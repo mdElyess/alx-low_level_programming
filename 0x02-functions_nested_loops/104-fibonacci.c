b7bac7da8d808efcf6162a861d0bac89d8eb3da9
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-24 01:15:41 +0100

    #advanced

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 
 /**
- * main - prints the sum of even-valued terms
+ * main - prints the first 98 Fibonacci numbers.
  * Return: Always 0
  **/
 
 int main(void)
 {
-	unsigned long long int i, nb1, nb2, aux;
+	unsigned long int i, nb1, nb2, j1, j2, k1, k2;
 
-	nb1 = 0;
-	nb2 = 1;
-	for (i = 0; i < 97; i++)
+	nb1 = 1;
+	nb2 = 2;
+	printf("%lu", nb1);
+	for (i = 0; i < 91; i++)
 	{
-		aux = nb2;
+		printf(", %lu", nb2);
 		nb2 += nb1;
-		nb1 = aux;
-		printf("%lu, ", nb2);
+		nb1 = nb2 - nb1;
 	}
-	aux = nb2;
-	nb2 += nb1;
-	printf("%lu\n", nb2);
+	j1 = nb1 / 1000000000;
+	j2 = nb1 % 1000000000;
+	k1 = nb2 / 1000000000;
+	k2 = nb2 % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
 	return (0);
 }
