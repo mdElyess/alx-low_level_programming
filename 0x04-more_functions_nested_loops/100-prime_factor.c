f4a498a476b1ee79040cee117ebeb6c538dbd0fd
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-26 00:18:00 +0100

    #advanced

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - prints the largest prime factor of the number 612852475143
- *
- * Return: Always 0
+ * main - print largest prime factor of 612852475143
+ * Return: 0 on success
  */
 
 int main(void)
 {
 	unsigned long num = 612852475143;
-	unsigned long div = 2;
+	unsigned long divisor = 2;
 
-	while(div < num)
+	while (divisor < num)
 	{
-		if (num % div == 0)
+		if (num % divisor == 0)
 		{
-			num /= div;
+			num /= divisor;
 			divisor = 2;
 		}
 		else
-			div++;
+			divisor++;
 	}
 	printf("%lu\n", num);
 	return (0);
