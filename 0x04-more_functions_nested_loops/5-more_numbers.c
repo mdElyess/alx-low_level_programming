6aaeaf793c04f7bf96b3ff4fc2f8448e04dc0643
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-25 22:56:47 +0100

    mandatory

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14.
+ * Return: return the result
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+		for (i = 0; i < 14; i++)
+			if (i / 10 != 0)
+				_putchar('1');
+			_putchar(i + '0');
+		_putchar('\n');
+}
