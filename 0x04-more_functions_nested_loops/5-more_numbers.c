dd2113d7adf4440b1a09cdc7bdf6f132d4cd9582
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-05-25 22:58:49 +0100

    mandatory

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,13 @@ void more_numbers(void)
 	int i, j;
 
 	for (j = 0; j < 10; j++)
+	{
 		for (i = 0; i < 14; i++)
+		{
 			if (i / 10 != 0)
 				_putchar('1');
 			_putchar(i + '0');
+		}
 		_putchar('\n');
+	}
 }
