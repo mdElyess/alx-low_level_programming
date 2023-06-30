7b1a44d924147dc142428506e53e27684604ac4e
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-06-30 20:43:22 +0100

    #advanced

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,7 +9,7 @@
  * Return: Always 0.
  */
 
-int main(int argc, char *argv)
+/*int main(int argc, char *argv)
 {
 	int bytes, index;
 	int (*address)(int, char **) = main;
@@ -40,5 +40,34 @@ int main(int argc, char *argv)
 	}
 	printf("\n");
 
+	return (0);
+}*/
+
+int main(int argc, char *argv)
+{
+	char *opc = (char *) main;
+	int i, nbytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+	printf("\n");
 	return (0);
 }
