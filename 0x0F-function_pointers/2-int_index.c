16445b9a4f4dfe7f2cf647e3bb4c7eac66c0a74e
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-06-30 20:19:42 +0100

    mandatory

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,13 +15,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-
-	for (index = 0; index < size; index++)
+	if (array && cmp)
 	{
-		if (cmp(array[0]) != 0)
-			return (index);
+		if (size <= 0)
+			return (-1);
+		for (index = 0; index < size; index++)
+			if (cmp(array[index]))
+				return (index);
 	}
 	return (-1);
 }
