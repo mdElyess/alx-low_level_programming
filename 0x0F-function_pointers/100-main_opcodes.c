4ed868d0921ed83af8b8b6abd7931c3244b36197
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-06-30 20:44:44 +0100

    #advanced

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,73 +1,39 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Prints the opcodes of itself.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
  * Return: Always 0.
  */
-
-/*int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-	int bytes, index;
-	int (*address)(int, char **) = main;
-	unsigned char opcode;
+char *opc = (char *) main;
+int i, nbytes;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	for (index = 0; index < bytes; index++)
-	{
-		opcode = *(unsigned char *) address;
-		printf("%.2x", opcode);
-
-		if (index == bytes - 1)
-			continue;
-
-		printf(" ");
-
-		address++;
-	}
-	printf("\n");
-
-	return (0);
-}*/
-
-int main(int argc, char *argv)
+if (argc != 2)
 {
-	char *opc = (char *) main;
-	int i, nbytes;
+printf("Error\n");
+exit(1);
+}
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
+nbytes = atoi(argv[1]);
 
-	nbytes = atoi(argv[1]);
+if (nbytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
 
-	if (nbytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
+for (i = 0; i < nbytes; i++)
+{
+printf("%02x", opc[i] & 0xFF);
+if (i != nbytes - 1)
+printf(" ");
+}
 
-	for (i = 0; i < nbytes; i++)
-	{
-		printf("%02x", opc[i] & 0xFF);
-		if (i != nbytes - 1)
-			printf(" ");
-	}
-	printf("\n");
-	return (0);
+printf("\n");
+return (0);
 }
