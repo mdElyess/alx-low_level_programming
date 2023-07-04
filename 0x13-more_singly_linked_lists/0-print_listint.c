7753fd07e292be5b810ec6abc5b4308e2c88bac9
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-07-04 13:37:02 +0100

    mandatory

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d", h->n);
 		h = h->next;
+		nodes++;
 	}
 	return (nodes);
 }
