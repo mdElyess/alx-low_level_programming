d9242ee501789f0c56f02cf2e71843d4217db269
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-07-06 15:46:06 +0100

    mandatory

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	for (len = 0; b[len];)
 		len++;
 
-	for (len -= 1; len >= 0; len --)
+	for (len -= 1; len >= 0; len--)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
